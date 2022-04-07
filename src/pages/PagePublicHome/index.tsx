c0be23d6eeb43ccf256eb021992b767145100d5a
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-04-07 20:11:58 -0300

    feat: styling initial screen

--- src/pages/PagePublicHome/index.tsx
+++ src/pages/PagePublicHome/index.tsx
@@ -1,36 +1,37 @@
-import { Button, HStack, Text, VStack } from "@chakra-ui/react";
+import { Button, HStack, Image, Text, VStack } from "@chakra-ui/react";
 
 export const PagePublicHome = () => {
   return (
     <VStack bg="gray.900" w="full" minH="100vh" color="white" spacing="0">
       <HStack
-        px="8"
         w="full"
         h="10vh"
         borderBottom="1px"
         borderColor="gray.700"
-        justify="space-between"
+        justify="center"
       >
-        <Text fontWeight="medium" color="cyan.300">
-          RemoteWorkr
-        </Text>
+        <HStack px="8" w="full" maxW="container.lg" justify="space-between">
+          <Text fontWeight="medium" color="cyan.300">
+            RemoteWorkr
+          </Text>
 
-        <Button colorScheme="cyan">Log In</Button>
+          <Button colorScheme="cyan">Log In</Button>
+        </HStack>
       </HStack>
 
-      <HStack w="full" px="8" justify="space-between">
-        <VStack
-          w="full"
-          align="flex-start"
-          height="90vh"
-          justify="center"
-          spacing="8"
-        >
+      <HStack
+        w="full"
+        maxW="container.lg"
+        justify="space-between"
+        height="70vh"
+        spacing="8"
+      >
+        <VStack px="8" w="full" align="flex-start" justify="center" spacing="8">
           <Text color="cyan.300" fontSize="5xl" maxW="450px">
-            O Melhor Amigo do Programador Remoto
+            A melhor maneira de gerenciar sua carreira remota.
           </Text>
-          <Text fontSize="lg" maxW="300px">
-            O RemotWorkr ajuda profissionais de tecnologia a gerenciar suas
+          <Text fontSize="md" maxW="300px">
+            O RemoteWorkr ajuda profissionais de tecnologia a gerenciar suas
             tarefas e criar timesheets com apenas um clique.
           </Text>
           <Button size="lg" colorScheme="cyan">
@@ -39,6 +40,7 @@ export const PagePublicHome = () => {
         </VStack>
 
         <VStack w="full">
+          <Image src="/developer-custom.svg" />
           <Text>O Melhor Amigo do Programador Remoto</Text>
         </VStack>
       </HStack>
