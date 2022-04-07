078f4a8e5a23471be4f25988cccbe1ccde4f1640
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-04-06 21:23:41 -0300

    feat: adjusting modal

--- src/components/BoardTasks/index.tsx
+++ src/components/BoardTasks/index.tsx
@@ -1,4 +1,23 @@
-import { HStack, Icon, IconButton, Text, VStack } from "@chakra-ui/react";
+import {
+  HStack,
+  IconButton,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Select,
+  Text,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react";
 import { HiOutlinePlus, HiOutlineTrash } from "react-icons/hi";
 import { Task } from "../Task";
 
@@ -7,50 +26,85 @@ interface Props {
 }
 
 export const BoardDayTasks = ({ day }: Props) => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
   return (
-    <VStack
-      w="full"
-      h="object-fit"
-      spacing="0"
-      style={{ outline: `solid 0.5px #E2E8F0` }}
-    >
-      <HStack
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent borderRadius={0}>
+          <ModalHeader>{day}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody as={VStack} w="full" px="4" py="8">
+            <Input type="text" placeholder="Task" />
+
+            <HStack w="full">
+              <NumberInput w="full" step={0.25} min={0} max={8}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+
+              <Select>
+                <option disabled hidden value="select">
+                  Select a Category
+                </option>
+                <option value="development">Development</option>
+                <option value="design">Design</option>
+                <option value="meetings">Meetings</option>
+                <option value="other">Other</option>
+              </Select>
+            </HStack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+      <VStack
         w="full"
-        bg="gray.600"
-        color="white"
-        p="2"
-        justify="space-between"
         h="object-fit"
+        spacing="0"
+        style={{ outline: `solid 0.5px #E2E8F0` }}
       >
-        <Text>{day}</Text>
-
-        <IconButton
-          variant="ghost"
-          icon={<HiOutlinePlus />}
+        <HStack
+          w="full"
+          bg="gray.600"
           color="white"
-          aria-label="Add Task"
-          borderRadius="0"
-          _hover={{ color: "gray.600", bg: "white" }}
-        />
-      </HStack>
+          p="2"
+          justify="space-between"
+          h="object-fit"
+        >
+          <Text>{day}</Text>
 
-      <VStack w="full" spacing="0" h="800px">
-        <Task
-          type="development"
-          duration={5}
-          task="3656 - Design new page with authorities and stuff"
-        />
-        <Task
-          type="design"
-          duration={3}
-          task="3656 - Design new page with authorities and stuff"
-        />
-        <Task
-          type="other"
-          duration={0.25}
-          task="3656 - Design new page with authorities and stuff"
-        />
+          <IconButton
+            variant="ghost"
+            icon={<HiOutlinePlus />}
+            color="white"
+            aria-label="Add Task"
+            borderRadius="0"
+            _hover={{ color: "gray.600", bg: "white" }}
+            onClick={onOpen}
+          />
+        </HStack>
+
+        <VStack w="full" spacing="0" h="800px">
+          <Task
+            type="development"
+            duration={5}
+            task="3656 - Design new page with authorities and stuff"
+          />
+          <Task
+            type="design"
+            duration={3}
+            task="3656 - Design new page with authorities and stuff"
+          />
+          <Task
+            type="other"
+            duration={0.25}
+            task="3656 - Design new page with authorities and stuff"
+          />
+        </VStack>
       </VStack>
-    </VStack>
+    </>
   );
 };
