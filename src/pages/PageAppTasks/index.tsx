cd7e847f2ee42f1c5b1b5fb7992459d175b39450
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-04-08 17:39:05 -0300

    chore: add supabase package and navigate to new page

--- src/pages/PageAppTasks/index.tsx
+++ src/pages/PageAppTasks/index.tsx
@@ -1,12 +1,26 @@
-import { Flex } from "@chakra-ui/react";
+import { Avatar, Button, Flex, HStack, VStack } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 import { Board } from "../../components/Board";
 import { SideBar } from "../../components/SideBar";
 
 export const PageAppTasks = () => {
+  const { push } = useRouter();
+
+  const handleNavigateTo = (path: string) => {
+    push(path);
+  };
   return (
     <Flex bg="gray.50" h="100vh">
       <SideBar />
-      <Board />
+      <VStack w="full" px="8">
+        <HStack h="10vh" w="full" justify="space-between">
+          <Avatar size="sm" name="Rafael Fischer" />
+          <Button onClick={() => handleNavigateTo("/app/tasks/task")}>
+            Add Task
+          </Button>
+        </HStack>
+      </VStack>
+      {/* <Board /> */}
     </Flex>
   );
 };
