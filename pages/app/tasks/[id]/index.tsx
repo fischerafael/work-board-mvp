eb1408bf31bc688f7690eccce2694a0d037af08c
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-04-08 17:40:56 -0300

    feat: add task page

---
+++ pages/app/tasks/[id]/index.tsx
@@ -0,0 +1,7 @@
+import { PageAppTask } from "../../../../src/pages/PageAppTask";
+
+const Task = () => {
+  return <PageAppTask />;
+};
+
+export default Task;
---
+++ src/pages/PageAppTask/index.tsx
@@ -0,0 +1,10 @@
+import { Flex } from "@chakra-ui/react";
+import { SideBar } from "../../components/SideBar";
+
+export const PageAppTask = () => {
+  return (
+    <Flex bg="gray.50" h="100vh">
+      <SideBar />
+    </Flex>
+  );
+};
